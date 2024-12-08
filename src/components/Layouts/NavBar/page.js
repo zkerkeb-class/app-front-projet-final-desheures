2f92febae6310e67a8a33ed28dff0b33127bd70a
TheLeon9 <leonfall09@gmail.com>
2024-12-08 17:16:06 +0100

    navbar light et dark

--- src/components/Layouts/NavBar/page.js
+++ src/components/Layouts/NavBar/page.js
@@ -1,22 +1,46 @@
-import React from 'react';
-// import Image from 'next/image';
+import React, { useState } from 'react';
 import style from './page.module.scss';
-// import Logo from 'images/logo/DesHeures_Logo.png';
 
 const NavBar = ({ darkMode, toggleDarkMode }) => {
+  const [language, setLanguage] = useState('FR');
+
+  const handleLanguageChange = (e) => {
+    setLanguage(e.target.value.toUpperCase());
+  };
+
   return (
-    <div className={style.navBar}>
-      {/* <div className={style.logo}>
-        <span>Des Heures</span>
+    <div className={`${style.nav_bar} ${darkMode ? style.dark : style.light}`}>
+      {/* App title */}
+      <h1 className={style.title_app}>Des Heures #{language}</h1>
+
+      {/* Search Bar */}
+      <div className={style.search}>
+        <input
+          className={style.search_input}
+          type="text"
+          placeholder="Rechercher..."
+        />
       </div>
 
-      <div className={style.search}>
-        <input type="text" placeholder="Rechercher..." />
-      </div> */}
+      <div className={style.nav_bar_button_wrapper}>
+        {/* Toggle for Language change */}
+        <div className={style.language_selector}>
+          <select
+            className={style.select_language}
+            onChange={handleLanguageChange}
+          >
+            <option value="FR">FR</option>
+            <option value="EN">EN</option>
+            <option value="PT">PT</option>
+          </select>
+        </div>
 
-      <button onClick={toggleDarkMode} className={style.darkModeToggle}>
-        {darkMode ? '☀️' : '🌙'}
-      </button>
+        {/* Toggle for Dark and Light mode */}
+        <label className={style.switch}>
+          <input type="checkbox" checked={darkMode} onChange={toggleDarkMode} />
+          <span className={style.slider}></span>
+        </label>
+      </div>
     </div>
   );
 };
