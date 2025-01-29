3f011f2658f6ab0be282c0872cfe66e331c43433
AndreCLAVERIA <andreclaveria2002@gmail.com>
2025-01-29 16:05:15 +0100

    change config and apckage.json

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,16 @@
 /** @type {import('next').NextConfig} */
 
-// import pkg from './next-i18next.config.js';
-// const { i18n } = pkg;
-
 const nextConfig = {
   reactStrictMode: false,
   images: {
-    domains: ['localhost'], // Ajouter localhost ici pour autoriser l'usage des images en local
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '3030',
+        pathname: '/**',
+      },
+    ],
   },
   // i18n,
 };
