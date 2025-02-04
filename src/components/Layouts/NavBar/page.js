ffac809c669e6a3437cf444593144c622580941b
TheLeon9 <leonfall09@gmail.com>
2025-02-04 10:35:08 +0100

    Test Husky

--- src/components/Layouts/NavBar/page.js
+++ src/components/Layouts/NavBar/page.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import style from './page.module.scss';
+import style from './page.module.scss'
 import { useTheme } from '@/context/ThemeContext.js'
 
 const NavBar = () => {
